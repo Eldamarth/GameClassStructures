be578991f199ee28a59e196a5dd985aa68868571
Eldamarth <james.a.kelley.mars@gmail.com>
2020-06-02 19:43:21 -0600

    Create basic Turn structure in Game.js

--- game/game.js
+++ game/game.js
@@ -3,11 +3,31 @@ const Map = require("./assets/map/map");
 const { Meter, SkillMeter } = require("./assets/meter/meter");
 
 class Turn extends BASE {
+	/* 
+    Turns have PHASES:
+        0 - PREP
+        1 - MAIN
+        2 - END
+
+    */
+
 	constructor(stateExpansion = {}) {
-		const state = {};
+		const state = {
+			phase: 0,
+			log: [],
+		};
 
 		super(state);
 		this.expandState(stateExpansion);
+	}
+	// GETTERS
+
+	// SETTERS
+
+	// METHODS
+	nextPhase() {
+		this.state.phase++;
+		return this.state.phase;
 	}
 }
 
@@ -21,4 +41,13 @@ module.exports = class Game extends BASE {
 		super(state);
 		this.expandState(stateExpansion);
 	}
+
+	// GETTERS
+	get party() {
+		return this.state.party;
+	}
+
+	// SETTERS
+
+	// METHODS
 };
